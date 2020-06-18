141fca8acdb79917bbaeedd13115042c3031536c
Aleksandra Aleksandrova <48100500+AleksandraAleksandrova@users.noreply.github.com>
2020-06-18 21:27:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import tkinter
+import ttk
+
+global price
+
+
+def create_choicebox(root, choices_list, label_name, position_row):
+    tkinter.Label(root, text=label_name).grid(row=position_row,column=0)
+    combobox = ttk.Combobox(root, values=choices_list)
+    combobox.grid(row=position_row, column=1)
+    combobox.bind("<<ComboboxSelected>>", callBackFunc)
+
+
+def read_movies():
+    movies_names = {}
+    with open("movies", 'r') as movies_file:
+        movies_list = movies_file.readlines()
+    for element in movies_list:
+        key = element.strip().split("-")[0]
+        val = element.strip().split("-")[1]
+        movies_names[key] = val
+    return movies_names
+
+
+def callBackFunc(event):
+    if event.widget._name == "!combobox2":
+        #print(event.widget.get())
+        #print(int(read_movies()[event.widget.get()]))
+        global price
+        price = (int(read_movies()[event.widget.get()]))
+        print("Price: ",price)
+
+
+#def print_price():
+#    global price
+#    print(price)
+
+
+def main():
+    root = tkinter.Tk()
+    root.geometry("400x200")
+    create_choicebox(root, ["1", "2", "3"], "Hall:", 0)
+    create_choicebox(root, list(read_movies().keys()), "Films:", 1)
+    create_choicebox(root, ["10:00", "12:00", "14:00"], "Hour:", 2)
+    create_choicebox(root, ["Child", "Adult"], "Ticket:", 3)
+    #tkinter.Button(root, text="Get price:", command=print_price()).grid(row=6, column=0)
+    tkinter.Button(root, text="Get price:").grid(row=6, column=0)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
